19421c3243451f20b5053fc963d70c7876582247
zhu-tom <zhutom01@gmail.com>
2019-12-19 22:51:50 -0500

    Update findProfs.py

--- findProfs.py
+++ findProfs.py
@@ -1,13 +1,14 @@
 import requests
 import ast
 
+f = open('profs.txt', 'r')
+profs = ast.literal_eval(f.readline())
+f.close()
+
 def findProf(name):
     name = name.split(" ")
     schoolId = "1420"
-    f = open('profs.txt', 'r')
-    profs = ast.literal_eval(f.readline())
-    f.close()
-
+    
     for prof in profs:
         if prof['tLname'] == name[-1]: # last name match
             if prof['tFname'][0] == name[0][0]: # first letter of first name match
