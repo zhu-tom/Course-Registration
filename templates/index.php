0342dd1bce98949a835d1c392195acb03c50eb00
zhu-tom <zhutom01@gmail.com>
2019-12-20 15:06:48 -0500

    Add timetables and paginations

--- templates/index.php
+++ templates/index.php
@@ -4,7 +4,28 @@
     <title>Enter Courses</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
+    <style>
+        table {
+            table-layout: fixed;
+        }
+        #timetable td {
+            color: white;
+        }
+        tbody tr {
+            height: 30px;
+        }
+        td h6, p {
+            margin: 0;
+            padding: 0;
+            border: 0;
+        }
+        .time {
+            font-weight: normal;
+        }
+        td {
+            vertical-align: center;
+        }
+    </style>
 </head>
 <body>
     <div class='container'>
@@ -40,17 +61,42 @@
                 </div>
             </div>
         </form>
-        <table class="table table-bordered">
+        <div class='row'>
+            <div class='col-3'>
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <li class="page-item">
+                        <a class="page-link" aria-label="Previous">
+                            <span aria-hidden="true" aria-label='Previous'>&laquo;</span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        </li>
+                        <input type='number' id='page'>
+                        <li class="page-item">
+                        <a class="page-link" aria-label="Next">
+                            <span aria-hidden="true" aria-label='Next'>&raquo;</span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class='col-2 align-middle text-left' id='totPages'>
+            </div>
+            <div class='col'>
+            </div>
+        </div>
+        <table class="table-sm table table-bordered">
             <thead>
                 <tr>
-                    <th scope="col">Time</th>
-                    <th scope="col">Sunday</th>
-                    <th scope="col">Monday</th>
-                    <th scope="col">Tuesday</th>
-                    <th scope="col">Wednesday</th>
-                    <th scope="col">Thursday</th>
-                    <th scope="col">Friday</th>
-                    <th scope="col">Saturday</th>
+                    <th class='text-center' style='width:60px' cope="col">Time</th>
+                    <th class='text-center' scope="col">Sunday</th>
+                    <th class='text-center' scope="col">Monday</th>
+                    <th class='text-center' scope="col">Tuesday</th>
+                    <th class='text-center' scope="col">Wednesday</th>
+                    <th class='text-center' scope="col">Thursday</th>
+                    <th class='text-center' scope="col">Friday</th>
+                    <th class='text-center' scope="col">Saturday</th>
                 </tr>
             </thead>
             <tbody id='timetable'>
@@ -73,11 +119,62 @@ codeRow = "<div class='row form-group'>\
                 </div>\
             </div>"
 delbtn = "<input type='button' style='width:100%' class='btn btn-danger del-course' value='Delete'>"
-$(document).ready(function(e) {
+function loadTimetable() {
+    $('#timetable').text('');
+    $('#timetable').append('<tr id="7.5"><th class="time align-middle text-right" rowspan="2" scope="col">8:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>');
     for (let i = 8; i <= 21; i++) {
-        $('#timetable').append('<tr id="'+ i + '"><th scope="col">' + i + ':00' + '</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>');
-        $('#timetable').append('<tr id="'+ (i+0.5) + '"><th scope="col">' + i + ':30' + '</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>');
+        $('#timetable').append('<tr id="'+ i + '"><th class="time align-middle text-right" style="display:none" scope="col"></th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>');
+        $('#timetable').append('<tr id="'+ (i+0.5) + '"><th class="time align-middle text-right" rowspan="2" scope="col">' + (i+1) + ':00' + '</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>');
     }
+}
+
+var timetables = null;
+var count = 0;
+
+function loadPage(pageNum) {
+    $('#page').val(pageNum+1);
+    console.log($('#page'));
+    result = timetables;
+    loadTimetable();
+    colours = ['#CC0000', '#FF8800', '#007E33', '#0099CC', '#00695c', '#0d47a1', '#9933CC', '#1C2331', '#3e2723', '#880e4f']
+    rows = $('#timetable').find('tr');
+    if (result['courses'][pageNum] === null) {
+        alert('No Timetables Found');
+        return;
+    }
+    for (lecture of result['courses'][pageNum]) {
+        if (lecture != null) {
+            startTime = lecture.time.values[0];
+            hour = Math.floor(startTime/100);
+            mins = (startTime % 100 == 35) ? 0.5 : 0;
+            startTime = hour + mins;
+
+            endTime = lecture.time.values[1];
+            hour = Math.floor(endTime/100);
+            mins = (endTime % 100 == 25) ? 0.5 : 1;
+            endTime = hour + mins;
+
+            colour = colours.pop();
+            rowspan = (endTime-startTime)/0.5;
+            startRow = $('[id="'+ startTime + '"]');
+            for (day of lecture.days) {
+                for (let i = startTime+0.5; i < endTime; i+=0.5) {
+                    $($('[id="'+ i + '"]').find('td')[day]).css('display', 'none');
+                }
+                $($(startRow).find('td')[day])
+                .attr('rowspan', rowspan)
+                .attr('id', lecture.code+lecture.section)
+                .css('background-color', colour)
+                .append('<h6>' + lecture.code + lecture.section + '</h6><p class="small">' + lecture.time.display + ' (' + lecture.crn + ')' + '</p><p class="small prof">'+ lecture.prof.name + (lecture.prof.ratings.overall_rating ? ' (' + lecture.prof.ratings.overall_rating + ')' : '') +'</p>');
+            }
+        }
+        
+    }
+}
+
+$(document).ready(function(e) {
+    loadTimetable();
+
     $.ajax({
         url: '/getTerms',
         type: 'POST',
@@ -95,8 +192,29 @@ $(document).ready(function(e) {
         $(e.target).remove();
         $('#codes').append(codeRow);
     });
+    $(document).on('input', '#page', (e) => {
+        count = $(e.target).val()-1
+        if (count < 0) {
+            count = timetables.courses.length - 1;
+        }
+        else if (count >= timetables.courses.length) {
+            count = 0;
+        }
+        loadPage(count);
+    });
     $(document).on('click', '.del-course', (e) => {
         $(e.target).parents('.row').remove();
+    });
+    $(document).on('click', '.page-link', (e) => {
+        count += $(e.target).attr('aria-label') == 'Next' ? 1 : -1;
+        if (count < 0) {
+            count = timetables.courses.length - 1;
+        }
+        else if (count >= timetables.courses.length) {
+            count = 0;
+        }
+        console.log(count);
+        loadPage(count);
     });
     $('#submit').on('click', (event) => {
         if ($('#term').val() === null) {
@@ -119,6 +237,9 @@ $(document).ready(function(e) {
             data: JSON.stringify(data),
             success: (result) => {
                 console.log(result);
+                $('#totPages').text('of ' + result.courses.length);
+                timetables = result;
+                loadPage(0);
             }
         });
     });
