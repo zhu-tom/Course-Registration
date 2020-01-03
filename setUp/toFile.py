2798fda83bb0cd6bd7663133fd2ef6c12737b1ec
zhu-tom <zhutom01@gmail.com>
2020-01-02 21:10:05 -0500

    Update profs.txt

--- setUp/toFile.py
+++ setUp/toFile.py
@@ -1,6 +1,6 @@
 import requests
 def profsToFile():
-    f = open('profs.json', 'w')
+    f = open('profs.txt', 'w')
     f.write('[')
     schoolId = "1420"
     url = f"http://www.ratemyprofessors.com/filter/professor/?&page=1&filter=teacherlastname_sort_s+asc&query=*%3A*&queryoption=TEACHER&queryBy=schoolId&sid={schoolId}"
